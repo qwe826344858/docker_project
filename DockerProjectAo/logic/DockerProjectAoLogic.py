a896cd806e9c9dea9e492bd6830aa6f4f92f111d
zoneslee <zoneslee@muchenglin.com>
2025-01-07 19:12:37 +0800

    feat:1.解决golang服务并发调用时go服务抛出的异常 "weakly-referenced object no longer exists"

--- DockerProjectAo/logic/DockerProjectAoLogic.py
+++ DockerProjectAo/logic/DockerProjectAoLogic.py
@@ -17,28 +17,31 @@ class DockerProjectAoLogic(Server):
 
     def GetItemInfo(self, request, context):
         Logger.info(f"GetItemInfo request:{request}")
-        filt ={"id": request.itemId}
-        dao = cs_item_dao.ItemDao()
-        ret, dataList = dao.getSingleInfo(filt)
-        if not ret:
-            Logger.info(f"查询失败!")
-            self.ErrCode = 1001
-            self.ErrMsg = "查询失败!"
-            resp = commpb.GetItemInfoResp(respHeader=self.getRespHeader())
-            return resp
+        try:
+            filt ={"id": request.itemId}
+            dao = cs_item_dao.ItemDao()
+            ret, dataList = dao.getSingleInfo(filt)
+            if not ret:
+                Logger.info(f"查询失败!")
+                self.ErrCode = 1001
+                self.ErrMsg = "查询失败!"
+                Resp = commpb.GetItemInfoResp(respHeader=self.getRespHeader())
+                return Resp
 
-        Logger.info(f"dataList:{dataList}")
-        resp = commpb.GetItemInfoResp(
-            respHeader=self.getRespHeader(),
-            id =dataList["id"],
-            itemSourceName =dataList["item_source_name"],
-            itemCnName = dataList["item_cn_name"],
-            sellOnlineCount = dataList["sell_online_count"],
-            picUrl = dataList["pic_url"],
-            prices = dataList["prices"],
-            currency = dataList["currency"],
-            addtime = dataList["addtime"],
-        )
-
-        Logger.info(f"resp:{resp}")
-        return resp
+            Logger.info(f"dataList:{dataList}")
+            Resp = commpb.GetItemInfoResp(
+                respHeader=self.getRespHeader(),
+                id =dataList["id"],
+                itemSourceName =dataList["item_source_name"],
+                itemCnName = dataList["item_cn_name"],
+                sellOnlineCount = dataList["sell_online_count"],
+                picUrl = dataList["pic_url"],
+                prices = dataList["prices"],
+                currency = dataList["currency"],
+                addtime = dataList["addtime"],
+            )
+            Logger.info(f"resp:{Resp}")
+        except Exception as e:
+            Logger.info(f"e:{e}")
+            Resp = commpb.GetItemInfoResp(respHeader=self.getRespHeader())
+        return Resp
